3451907329bd2e5499bd1729f3b83fe12537ae97
submarine <roguexsubmarine@gmail.com>
2024-07-06 00:46:58 +0530

    added files

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,38 @@
+
+export default function GameBoard({ onSelectSquare, board}) {
+
+
+
+    // const [gameBoard, setGameBoard] = useState(INITIAL_GAME_BOARD);
+
+    // function handleSelectSquare(rowIndex, colIndex) {
+    //     setGameBoard((prevGameBoard) => {
+    //         const updateBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+    //         updateBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updateBoard;
+    //     });
+
+    //     onSelectSquare();
+    // }
+
+    return (
+        <ol id="game-board">
+            {board.map((row, rowIndex) => (
+                <li key={rowIndex}>
+                    <ol>
+                        {row.map((playerSymbol, colIndex) =>(
+                            <li key={colIndex}>
+                                <button
+                                    onClick={() => onSelectSquare(rowIndex, colIndex)}
+                                    disabled={playerSymbol !== null}
+                                >
+                                    {playerSymbol}
+                                </button>
+                            </li>
+                        ))}
+                    </ol>
+                </li>
+            ))}
+        </ol> 
+    );
+}
